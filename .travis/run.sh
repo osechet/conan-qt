a4bd9f052d5bd4228fd0167d4dfcbd582a50b40b
osechet <osechet@gmail.com>
2016-12-15 10:41:49 +0100

    Restart dev from qt-base

---
+++ .travis/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [[ "$(uname -s)" == 'Darwin' ]]; then
+    if which pyenv > /dev/null; then
+        eval "$(pyenv init -)"
+    fi
+    pyenv activate conan
+fi
+
+# Use travis_wait since the command may not output text for more than 10 minutes
+python build.py
