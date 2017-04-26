8788bcfc9f9697225bacb80fcace711bc9aab023
osechet <osechet@gmail.com>
2017-04-26 14:14:59 +0200

    Switch to bintray repo and enable Mac OS builds fixes #8

--- build.py
+++ build.py
@@ -1,15 +1,21 @@
-from conan.packager import ConanMultiPackager
-from conans.tools import os_info
-import platform
 
-if __name__ == "__main__":
+from conan.packager import ConanMultiPackager
+
+def main():
+    """
+    Main function.
+    """
     builder = ConanMultiPackager(username="osechet", channel="stable")
     builder.add_common_builds()
     filtered_builds = []
-    for settings, options in builder.builds:
-        if os_info.is_macos and settings["build_type"] != "Debug":
-            filtered_builds.append([settings, options])
-        if settings["arch"] == "x86_64":
-             filtered_builds.append([settings, options])
+    for settings, options, env_vars, build_requires in builder.builds:
+        if settings["arch"] != "x86_64":
+            continue
+
+        filtered_builds.append([settings, options, env_vars, build_requires])
+
     builder.builds = filtered_builds
     builder.run()
+
+if __name__ == "__main__":
+    main()
