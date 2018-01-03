f83ad66029f75dc1fd9a974a298071f9a27fc772
Lars Bilke <lars.bilke@ufz.de>
2018-01-03 12:28:41 +0100

    Build libstdc++11 as well (pure_c=False).

--- build.py
+++ build.py
@@ -2,20 +2,14 @@ from conan.packager import ConanMultiPackager
 import copy
 
 def main():
-    """
-    Main function.
-    """
-
-    builder = ConanMultiPackager(username="osechet", channel="stable")
-    builder.add_common_builds()
+    builder = ConanMultiPackager(username="osechet", archs = ["x86_64"])
+    builder.add_common_builds(pure_c=False)
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
         if settings["compiler"] == "Visual Studio":
             if settings["compiler.runtime"] == "MT" or settings["compiler.runtime"] == "MTd":
                 # Ignore MT runtime
                 continue
-        if settings["arch"] != "x86_64":
-            continue
 
         new_options = copy.copy(options)
         new_options["Qt:xmlpatterns"] = True
